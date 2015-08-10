2d32380debf0bc7b3c69d05fc0129d3dc8112249
max-ilse <maximilian.ilse@gmail.com>
2015-08-11 01:31:20 +0200

    Update of the 3 files in the web folder

--- web/script.js
+++ web/script.js
@@ -1,7 +1,20 @@
 // Submit
 function submit() {
 	// Get data from text field
-	var text = $('#textInput').val();
+	var text = $("#textInput").val();
+
+	// Looking for input errors
+	if (text == null || text == "") { // Empty input
+        alert("Das Textfeld darf nicht leer sein!");
+        return false;
+    } else if (/[a-zA-Z0-9\\., \\ä\\ü\\ö\\ß\\Ä\\Ü\\Ö]{1,75}$/.test(text) == false) { // Special Characters
+    	alert("Bitte benutze nur 0-9a-zA-ZäöüßÄÖÜ .,");
+        return false;
+    } else if (text.length > 75) { // Input is too long
+    	alert("Fasse dich bitte etwas kürzer!");
+        return false;
+    }
+
 
 	// Activate hour glass and grey out the page
 	$("#overlay").css("cursor", "wait");
@@ -13,6 +26,7 @@ function submit() {
 		data: JSON.stringify({'primetext':text}),
 		contentType:"application/json; charset=utf-8",
 		dataType:"json",
+		timeout: 10000,
 		success: function(data){
 			// Update text field
 			$("#textInput").val(data.poem);
@@ -20,6 +34,9 @@ function submit() {
 			// No more waiting
 			$("#overlay").css("cursor", "default");
 			$("#overlay").fadeOut(10);
+		},
+		error: function(status){
+			alert("Timeout!");
 		}
 	})
 }
